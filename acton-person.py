604b6f704135544adac3f8fb624b75cb73d618d7
Danilo <digital.danmcnd@gmail.com>
2026-02-04 21:57:12 -0300

    desafio-if-match

---
+++ acton-person.py
@@ -0,0 +1,53 @@
+'''
+📋 Descrição da Tarefa:
+Você está criando um pequeno sistema de um jogo de aventura onde o jogador será classificado por sua experiência e,
+com base em sua escolha, executará uma ação dentro do jogo.
+ 
+🔧 O que seu programa deve fazer:
+1.Pedir ao jogador quantos pontos de experiência ele tem (XP):
+Menos de 100 → "Iniciante" 
+Entre 100 e 500 → "Intermediário"
+Mais de 500 → "Veterano"
+ 
+Use if/elif/else para essa classificação.
+ 
+2. Depois, o programa deve perguntar qual ação o jogador deseja executar (usar match case): 
+"A" → Atacar
+"D" → Defender
+"F" → Fugir
+ 
+Qualquer outra tecla → "Ação inválida"
+ 
+Mostre uma mensagem apropriada para cada ação, como: 
+"Você avançou para o ataque!"
+"Você levantou o escudo!"
+"Você fugiu da batalha!"
+ 
+📝 Regras de Entrega:
+Crie seu código em um arquivo .py
+Faça testes com diferentes níveis de XP e ações
+Envie o código por GitHub ou por sua plataforma de aulas
+'''
+
+player_name = input("Digite o nome do jogador: ")
+player_xp = int(input(f"Quantos pontos de experiencia {player_name} possui ? "))
+
+if player_xp <= 100:
+    print(f"O jogador {player_name} é Iniciante")
+elif player_xp <= 500:
+    print(f"O jogador {player_name} é Intermediário")
+else:
+    print(f"O jogador {player_name} é Veterano")
+
+
+action = input("Qual ação deseja realizar ?\n[A] Atacar\n[D] Defender\n[F] Fugir\n Vamos la: ")
+match action:
+    case "A" | "a" | "Atacar" | "atacar":
+        print(f"{player_name} avançou para o ataque!")
+    case "D" | "d" | "Defender" | "defender":
+        print(f"{player_name} levantou o escudo!")
+    case "F" | "f" | "Fugir" | "fugir":
+        print(f"{player_name} fugiu da batalha!")
+    case _:
+        print("Ação inválida")
+
